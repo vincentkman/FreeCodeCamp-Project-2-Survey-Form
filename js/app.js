5e07a4a6c18e6fa2a844c9dcb9f54fae0f8ea308
Vincent Man <vincentman2702@hotmail.com>
2019-04-23 12:35:37 +0100

    Update project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	let x = 0;
+	let y = 0;
+
+	const banner = $('#banner');
+
+	banner.css('backgroundPosition', x + 'px' + ' ' + y + 'px');
+
+	window.setInterval(function() {
+		banner.css('backgroundPosition', x + 'px' + ' ' + y + 'px');
+		y--;
+
+	}, 90);
+}) (jQuery);
